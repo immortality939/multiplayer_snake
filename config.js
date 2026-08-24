7690f2a3adb5f9536655e0cd4eea20e4d7989bc7
immortality939 <49891884+immortality939@users.noreply.github.com>
2026-08-24 23:40:41 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const GAME_CONFIG = {
   initialPosition: { x: 10, y: 5 }, // starting head position
 
   // Movement & speed
-  speedMs: 120,                   // game loop interval in milliseconds (lower = faster)
+  speedMs: 120l,                   // game loop interval in milliseconds (lower = faster)
 
   // Grid & board
   grid: {
